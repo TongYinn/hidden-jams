90fe7ad5fe5fb843d8d338069b64b7d4716a4081
Tong Yin <71139027+TongYinn@users.noreply.github.com>
2023-09-01 17:30:48 -0400

    added navbar and footer

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+
+const NavBar = () => {
+  return (
+    <Nav>
+      <NavItem>home</NavItem>
+    </Nav>
+  );
+};
+
+export default NavBar;
